935469cc4920d83bcec483693373105db15d95fa
Moheed789 <moheedsiddiqui74@gmail.com>
2023-10-31 18:15:07 -0700

    Backend Programs

---
+++ Nodejs_API/index.js
@@ -0,0 +1,99 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+const app = express();
+
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+
+const isAuthenciated = async(req, res, next) => {
+    const {token} = req.cookies;
+    if (token) {
+        const decoded = jwt.verify(token, "abcd");
+        req.user = await User.findById(decoded._id);
+        next()
+    } else {
+        res.redirect("/login");
+    }
+}
+
+app.get("/",isAuthenciated, (req, res) => {
+    res.render("logout");
+})
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
+app.get("/register", (req, res) => {
+    res.render("register");
+})
+
+app.post("/login", async(req, res) => {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if (!user) {
+        return res.redirect("/register");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res.render("login", {email, message: "Incorrect Password"});
+    }
+    const token = jwt.sign({id: user._id}, "abcd");
+    res.cookie("token", token,{
+        httpOnly: true,
+        expires: new Date(Date.now()+60*1000)
+    });
+    res.redirect("/");
+})
+
+app.post("/register", async(req, res) => {
+    const {name, email, password} = req.body;
+    let user = await User.findOne({email});
+    if (user) {
+        return res.redirect("/login");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user = await User.create({name, email, password: hashedPassword});
+    const token = jwt.sign({id: user._id}, "abcd");
+    res.cookie("token", token,{
+        httpOnly: true,
+        expires: new Date(Date.now()+60*1000)
+    });
+    res.redirect("/");
+})
+
+app.get("/logout", (req, res) => {
+    res.cookie("token", null,{
+    httpOnly: true,
+    expires: new Date(Date.now())
+    });
+    res.redirect("/");
+})
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("Database Connected")
+    app.listen(5000, () => {
+        console.log("Server is working");
+    })
+})
+.catch((err) => {
+    console.log(err)
+});
