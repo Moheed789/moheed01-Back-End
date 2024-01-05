5a183858c2f81d06ea46e2d5c28f371340f8022c
Moheed789 <moheedsiddiqui74@gmail.com>
2024-01-04 16:50:37 -0800

    Include relationships

--- TwitterBackend/src/routes/userRoutes.ts
+++ TwitterBackend/src/routes/userRoutes.ts
@@ -32,14 +32,24 @@ router.post("/", async(req, res) => {
 
 // Get Users List
 router.get("/", async(req, res) => {
-    const allUsers = await prisma.user.findMany()
+    const allUsers = await prisma.user.findMany({
+        // select: {
+        //     id: true,
+        //     name: true, 
+        //     image: true,
+        //     bio: true
+        // }
+    })
     res.json(allUsers)
 })
 
 // Get One User
 router.get("/:id", async(req, res) => {
     const {id} = req.params;
-    const user = await prisma.user.findUnique({where: {id: Number(id)}})
+    const user = await prisma.user.findUnique({
+        where: {id: Number(id)},
+        include: {tweets: true}
+    })
     res.json(user)
 })
 
